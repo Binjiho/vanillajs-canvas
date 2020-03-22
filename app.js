4ad730abfca369306f54e473b6e19123420debdb
Binjiho <jiyu1948@gmail.com>
2020-03-22 18:23:59 +0900

    Mouse Event
    2D Context

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
-function init(){
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
+canvas.width=500;
+canvas.height=500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.55;
+
+let painting = false;
+
+function startPainting(){
+    painting = true;
 }
 
-init();
+function stopPainting(){
+    painting = false;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else {
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+
+if (canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
+
+// function init(){
+
+// }
+
+// init();
